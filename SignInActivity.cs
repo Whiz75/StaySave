f58586225875708e280525adddf22e1e7404ed45
Whiz75 <75149500+Whiz75@users.noreply.github.com>
2021-05-27 17:35:43 +0200

    init commit Add project files.

---
+++ SignInActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StaySave
+{
+    [Activity(Label = "Activity1")]
+    public class SignInActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.sign_in_acticity);
+           
+        }
+    }
+}
